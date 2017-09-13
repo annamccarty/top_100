f758a8bcb16a311a174b2ff0814b65e8a88dfcd1
Anna McCarty <anna.mccarty89@gmail.com>
2017-09-12 21:01:27 -0600

    order songs by rank

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   before_action :set_song, only: [:show, :edit, :update, :destroy]
   def index
-    @songs = Song.all
+    @songs = Song.all.order(:rank)
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
 genre = ['Rock', 'Country', 'Pop', 'Rap', 'Metal', 'Alternative']
 
-
+rank = 1
 100.times do
   Song.create(
     title: "#{Faker::Lorem.word} #{Faker::Lorem.word} #{Faker::Lorem.word} #{Faker::Lorem.word}",
     artist: Faker::Name.name,
-    rank: [1..100].sample,
+    rank: rank,
     genre: genre.sample
 
   )
+  rank += 1
 end
 
 puts "data has been seeded"
