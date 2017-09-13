10245ace0eae7d7156e23190e0a211b2e89d7266
Anna McCarty <anna.mccarty89@gmail.com>
2017-09-12 20:47:37 -0600

    add CRUD actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ genre = ['Rock', 'Country', 'Pop', 'Rap', 'Metal', 'Alternative']
 
 100.times do
   Song.create(
-    title: Faker::Lorem.words(4),
+    title: "#{Faker::Lorem.word} #{Faker::Lorem.word} #{Faker::Lorem.word} #{Faker::Lorem.word}",
     artist: Faker::Name.name,
     rank: [1..100].sample,
     genre: genre.sample
